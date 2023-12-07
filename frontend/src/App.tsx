b26cf4e3b93519b39c0e96b3b9eaa6329ee31fe8
suhwa <tnghk9611@naver.com>
2023-12-07 16:34:32 +0900

    chore: eslint 설정
    이슈 번호: #3
    
    - eslint 패키지 설치
    - typescript에서도 사용하도록 추가 패키지 설치

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App" />;
 }
 
 export default App;
