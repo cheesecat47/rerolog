1bab64e1b89de3cedc804c52f318a12e7609dc13
cheesecat47 <cheesecat47@gmail.com>
2023-12-08 18:01:49 +0900

    Feat: 상단 Navbar 구현
    이슈 번호: #13
    
    - tailwind navbar 템플릿을 기반으로 상단 Navbar 구현
      - https://v1.tailwindcss.com/components/navigation

--- myBlog-frontend/src/components/NavbarTop.vue
+++ myBlog-frontend/src/components/NavbarTop.vue
@@ -1,21 +1,19 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
 
 <template>
-  <div>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-    Blog Title
-  </div>
-
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
+  <header class="box-border">
+    <nav class="flex items-center justify-between flex-wrap p-8">
+      <div class="flex items-center flex-shrink-0 mr-6">
+        <span class="font-bold text-2xl">Blog Title</span>
+      </div>
+      <div class="block lg:flex lg:items-center lg:w-auto">
+        <RouterLink to="/">로그인</RouterLink>
+      </div>
+    </nav>
+    <hr />
+  </header>
 </template>
 
 <style scoped></style>
