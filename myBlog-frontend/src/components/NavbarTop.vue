71657e7f0d7b3eeed3d945f8d2e126e397fb0786
cheesecat47 <cheesecat47@gmail.com>
2023-12-08 16:32:12 +0900

    Feat: 페이지 구조 생성
    이슈 번호: #13
    
    - 페이지 구조 생성
      - container 너비 lg(1024px)로 제한
      - 상단 Navbar, 본문, Footer로 구성

---
+++ myBlog-frontend/src/components/NavbarTop.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    Blog Title
+  </div>
+
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
---
+++ myBlog-frontend/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        Footer
+    </div>
+</template>
+
+<style scoped>
+
+</style>
