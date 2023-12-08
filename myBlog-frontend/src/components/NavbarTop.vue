ebc07d22fe72ec1b8956a5e7c938f6be3744d24f
cheesecat47 <cheesecat47@gmail.com>
2023-12-08 18:30:22 +0900

    Fix: 로그인 상태에 따라 상단 Navbar 표시 항목 변경
    이슈 번호: #13
    
    - 상단 Navbar 우측 항목으로
      - 로그인 안 된 경우 '로그인' 표시
      - 로그인 된 상태는 '마이페이지', '로그아웃' 표시
      - '로그아웃' 클릭시 로그인 안 된 경우로 바뀌도록 간단하게 로직 추가

--- myBlog-frontend/src/components/NavbarTop.vue
+++ myBlog-frontend/src/components/NavbarTop.vue
@@ -1,5 +1,15 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { useUserStore } from '../stores/user.js';
+import { storeToRefs } from 'pinia';
+
+const userStore = useUserStore();
+const { isLogin } = storeToRefs(userStore);
+
+const logout = () => {
+  isLogin.value = false;
+  alert('logout');
+};
 </script>
 
 <template>
@@ -8,8 +18,12 @@ import { RouterLink } from 'vue-router';
       <div class="flex items-center flex-shrink-0 mr-6">
         <span class="font-bold text-2xl">Blog Title</span>
       </div>
-      <div class="block lg:flex lg:items-center lg:w-auto">
+      <div v-if="!isLogin" class="block lg:flex lg:items-center lg:w-auto">
         <RouterLink to="/">로그인</RouterLink>
+      </div>
+      <div v-if="isLogin" class="block lg:flex lg:items-center lg:w-auto">
+        <RouterLink to="/" class="mr-6">마이페이지</RouterLink>
+        <span @click="logout">로그아웃</span>
       </div>
     </nav>
     <hr />
