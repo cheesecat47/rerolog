29ccfe1617df08a076443b8ed45213e5b9d79c4c
cheesecat47 <cheesecat47@gmail.com>
2023-12-08 19:29:09 +0900

    Feat: Footer 구현
    이슈 번호: #13
    
    - Footer 구현
    - 블로그 주인(유저)의 연락 정보를 표시할 때 API로 받아와서 동적으로
      처리 필요.

--- myBlog-frontend/src/components/TheFooter.vue
+++ myBlog-frontend/src/components/TheFooter.vue
@@ -1,13 +1,23 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div>
-        Footer
+  <footer class="footer items-center">
+    <hr />
+    <div class="flex items-center justify-evenly mt-2">
+      <div>
+        <span> Blog Title &copy; 2023 Shin Juyong. </span>
+      </div>
+      <div class="flex items-center mr-6">
+        <span class="mr-6">Contact</span>
+        <ul>
+          <!-- Todo: 여기 들어갈 내용은 추후 API로 받아온 유저 정보 객체에 따라 동적으로 처리 필요. -->
+          <li><a href="mailto:cheesecat47@gmail.com">cheesecat47@gmail.com</a></li>
+          <li><a href="https://github.com/cheesecat47">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/shinjuyong">LinkedIn</a></li>
+        </ul>
+      </div>
     </div>
+  </footer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
