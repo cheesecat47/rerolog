fbff50ee32c613d52c0f889255de348a9b209b78
suhwa <tnghk9611@naver.com>
2023-12-12 11:05:48 +0900

    feat: user 정보 받아오기 테스트 코드 작성
    이슈 번호: #4
    
    - axios로 통신하여 user/1로 이동했을 때 1번 유저 정보를 받아오는지 확인

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,22 @@
-const MainPage = () => {
-    return <div>메인페이지</div>;
-};
+import axios from 'axios';
+
+function MainPage() {
+    const handleClick = async () => {
+        try {
+            const response = await axios.get('http://localhost:8080/user/1');
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div>
+            <button type="button" onClick={handleClick}>
+                유저 페이지로
+            </button>
+        </div>
+    );
+}
 
 export default MainPage;
