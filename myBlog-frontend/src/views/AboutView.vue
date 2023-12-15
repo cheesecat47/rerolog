799f7a4c6f0fcb5c8e4bd8b70907a71d3f4f25a3
cheesecat47 <cheesecat47@gmail.com>
2023-12-15 21:46:57 +0900

    Feat: 소개 페이지 구현
    이슈 번호: #23
    
    - About 페이지 구현
    - 화면 설계서 이미지를 실제 구현 스크린샷으로 교체

--- myBlog-frontend/src/views/AboutView.vue
+++ myBlog-frontend/src/views/AboutView.vue
@@ -1,38 +1,60 @@
 <script setup>
-import axios from "axios";
-import {ref} from "vue";
+import { ref } from 'vue';
 
-const resp = ref({});
-const board_list = ref([]);
-
-axios.get("http://localhost:8080/board").then(({data}) => {
-  resp.value = data;
-  console.log(resp.value);
-  board_list.value = data.data;
-  console.log(data.data, board_list.value);
+// Todo: 추후 API 응답 데이터로 변경 필요.
+const user = ref({
+  id_str: 'cheesecat47',
+  name: '신주용',
+  content:
+    '안녕하세요, 신주용입니다. 저는 고양이를 좋아합니다. 사실 이런 곳에는 Lorem Ipsum 문자열을 채워넣으면 가장 편하긴 한데 그러기에는 재미가 없잖아요. 근데 안그러려니 딱히 채울 내용이 없긴 해요. 그래서 한 세 줄만 넣어봐야겠어요. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum, nunc at posuere iaculis, augue lectus efficitur dolor, ac facilisis metus ipsum ac sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In hac habitasse platea dictumst. Nulla elementum facilisis velit, quis varius ante venenatis nec.',
+  contacts: [
+    { type: 'Email', value: 'cheesecat47@gmail.com' },
+    { type: 'GitHub', value: 'https://github.com/cheesecat47' },
+    { type: 'LinkedIn', value: 'https://www.linkedin.com/in/shinjuyong' },
+  ],
 });
+
+const contactIcon = (t) => {
+  switch (t) {
+    case 'Email':
+      return ['fas', 'envelope'];
+    case 'GitHub':
+      return ['fab', 'github'];
+    case 'LinkedIn':
+      return ['fab', 'linkedin'];
+    default:
+      return ['fas', 'link'];
+  }
+};
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div v-if="board_list.length > 0">
-      <ul v-for="(item, index) in board_list" :key="index">
-        <li>item: {{ item }}</li>
-      </ul>
+  <main class="mx-6 lg:mx-0 flex flex-col">
+    <div class="hero mx-auto max-w-xl min-w-lg">
+      <div class="hero-content flex-row">
+        <img
+          class="rounded-lg shadow-lg w-40"
+          src="../assets/6B152AA5-CC62-49B6-913A-7C702AF22F1E_1_102_o.jpeg"
+          alt="profile image"
+        />
+        <div class="flex flex-col items-center pl-6 gap-2">
+          <h1 class="text-3xl font-bold">{{ user.name }}</h1>
+          <h2 class="text-xl">{{ user.id_str }}</h2>
+          <ul class="mt-4">
+            <li
+              class="text-sm"
+              v-for="(item, index) in user.contacts"
+              :key="index"
+            >
+              <a :href="(item.type === 'Email' ? 'mailto:' : '') + item.value">
+                <font-awesome-icon :icon="contactIcon(item.type)" />
+                {{ item.value }}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div v-else>
-      <p>표시할 항목이 없습니다.</p>
-    </div>
-  </div>
+    <section class="mt-6 max-w-2xl mx-auto">{{ user.content }}</section>
+  </main>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
