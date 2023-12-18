bd4dd7d2bf1117365e68e52eb919b7120db1e716
suhwa <tnghk9611@naver.com>
2023-12-19 02:45:29 +0900

    refactor: header 컴포넌트 리팩토링
    이슈 번호: #9
    
    - 헤더에 있는 아이콘, 텍스트를 HeaderIconText 컴포넌트로 분리

---
+++ frontend/src/components/common/HeaderIconText/index.ts
@@ -0,0 +1,1 @@
+export { default as HeaderIconText } from './HeaderIconText';
--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -1,18 +1,16 @@
 // import React, { useEffect, useState } from 'react';
 
-import flower from '../../assets/icons/ML_flower-icon.png';
-import login from '../../assets/icons/ML_login_icon.png';
-import logout from '../../assets/icons/ML_logout-icon.png';
-import moon from '../../assets/icons/ML_moon-icon.png';
-import sun from '../../assets/icons/ML_sun-icon.png';
-import my from '../../assets/icons/ML_my-icon.png';
-import pencil from '../../assets/icons/ML_pencil-icon.png';
+import flower from '../../../assets/icons/ML_flower-icon.png';
+import login from '../../../assets/icons/ML_login_icon.png';
+import logout from '../../../assets/icons/ML_logout-icon.png';
+import moon from '../../../assets/icons/ML_moon-icon.png';
+import sun from '../../../assets/icons/ML_sun-icon.png';
+import my from '../../../assets/icons/ML_my-icon.png';
+import pencil from '../../../assets/icons/ML_pencil-icon.png';
 
-type blogNameType = {
-    blogName: string;
-}
+import { HeaderIconText } from "../../common/HeaderIconText";
 
-const Header = ({ blogName }: blogNameType) => {
+const Header = ({ blogName }: { blogName: string }) => {
     // 추후 로그인 여부 확인
     // const [isLogin, setIsLogin] = useState(false);
 
@@ -21,40 +19,28 @@ const Header = ({ blogName }: blogNameType) => {
     }
 
     return (
-        <header className="w-full h-16 grid grid-cols-11 place-items-center shadow-md font-PyeongChangPeace">
-            <div className="w-full col-span-5 bg-white" />
-            <div className="w-full col-span-1 flex justify-center items-center">
+        <header className="w-full h-16 flex bg-transparent font-PyeongChangPeace ">
+            <div className="basis-[40%]" />
+            <div className="basis-[20%] flex justify-center items-center">
                 <img className="w-8" src={flower} alt="logo" />
                 <span className="text-gray-700 font-semibold text-lg pl-1">{blogName}</span>
             </div>
-            <div className="w-full col-span-5 flex justify-end">
+            <div className="basis-[40%] flex justify-end">
                 {
-                    checkIsLogin() ? <div className="invisible md:visible w-full flex justify-end">
-                        <div className="flex justify-center items-center w-16">
-                            <img className="w-6" src={pencil} alt="pencil" />
-                            <span className="text-gray-600 font-mediun text-xs pl-1">Write</span>
-                        </div>
-                        <div className="flex justify-center items-center w-16">
-                            <img className="w-6" src={my} alt="my" />
-                            <span className="text-gray-600 font-mediun text-xs pl-1">My</span>
-                        </div>
-                        <div className="flex justify-center items-center w-16">
-                            <img className="w-4" src={logout} alt="logout" />
-                            <span className="text-gray-600 font-mediun text-xs pl-1">Logout</span>
-                        </div>
+                    checkIsLogin() ? <div className="hidden sm:flex w-full justify-end">
+                        <HeaderIconText icon={pencil} alt="pencil" text="Write" />
+                        <HeaderIconText icon={my} alt="my" text="My" />
+                        <HeaderIconText icon={logout} alt="logout" text="Logout" />
                     </div>
                         :
-                        <div className="col-span-1 flex justify-center items-center">
-                            <img className="w-8" src={login} alt="login" />
-                            <span className="text-gray-600 font-mediun text-xs">Logout</span>
-                        </div>
+                        <HeaderIconText icon={login} alt="login" text="Login" />
                 }
-                <div className="flex justify-center items-center mx-4">
-                    <img className="w-6" src={sun} alt="sun" />
-                    <div className="w-24 rounded-full mx-2 bg-gray-100">
+                <div className="hidden md:flex justify-center items-center mr-4">
+                    <img className="w-5" src={sun} alt="sun" />
+                    <div className="w-24 rounded-full mx-1 bg-gray-100">
                         &nbsp;
                     </div>
-                    <img className="w-6" src={moon} alt="moon" />
+                    <img className="w-5" src={moon} alt="moon" />
                 </div>
             </div>
         </header>
