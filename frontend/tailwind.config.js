ec2806aeffe1e7ce1ddd20ba15579a2e8841e7fa
suhwa <tnghk9611@naver.com>
2023-12-27 01:14:09 +0900

    chore: tailwind custom color 추가
    이슈 번호: #14
    
    - font color, borderRadius 속성 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
         // src 하위 파일 중 확장자가 .js,.jsx,.ts,.tsx인 파일을 대상으로 한다는 의미
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                lightGray: '#FAFAFA',
+                'ml-pink': {
+                    100: '#FFF4F4',
+                    200: '#FCD4D4',
+                    300: '#FFBDBD',
+                    400: '#FF999A',
+                },
+            },
+            borderRadius: {
+                10: '10px',
+                20: '20px',
+            },
+        },
         fontFamily: {
             PyeongChangPeace: ['PyeongChangPeace'],
         },
