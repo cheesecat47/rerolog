619fca00adfda2bed9070f53996d382176df29f1
suhwa <tnghk9611@naver.com>
2023-12-27 01:37:21 +0900

    feat: useInput custom hooks 구현
    이슈 번호: #10
    
    - input state값을 관리하는 custom hooks 추가

---
+++ frontend/src/hooks/useInput/useInput.tsx
@@ -0,0 +1,18 @@
+import { useState, ChangeEvent } from 'react';
+
+interface State {
+    [key: string]: string | number;
+}
+
+const useInput = (initialValue: State) => {
+    const [values, setValues] = useState(initialValue);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value });
+    };
+
+    return { values, handleChange };
+};
+
+export default useInput;
