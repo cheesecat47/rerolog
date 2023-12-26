dcd56f730c3ae294a579ca8842468f33e2684433
suhwa <tnghk9611@naver.com>
2023-12-27 01:26:42 +0900

    feat: CategoryBadge 컴포넌트 구현
    이슈 번호: #14

--- frontend/src/components/common/CategoryBadge/CategoryBadge.tsx
+++ frontend/src/components/common/CategoryBadge/CategoryBadge.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CategoryBadge = () => {
+const CategoryBadge = ({ categoryName }: { categoryName: string }) => {
     return (
-        <div>
-            CategoryBadge
+        <div className="text-xs mr-4 px-2 bg-ml-pink-100 rounded-full text-gray-700">
+            {categoryName}
         </div>
     );
 }
