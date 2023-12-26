d3b71e6d922d595d3ec9d953663238df1ca58ed3
suhwa <tnghk9611@naver.com>
2023-12-27 01:27:11 +0900

    feat: PostList 컴포넌트 구현
    이슈 번호: #14

--- frontend/src/components/post/PostList/PostList.tsx
+++ frontend/src/components/post/PostList/PostList.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { POST_MODE } from "../../../constants/post";
-import { PostModeType } from "../../../types/model";
+// import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { PostCard } from "../PostCard";
+import { POST_OPTION } from "../../../constants/post";
 
-const PostList = ({ mode }: PostModeType) => {
+import { Post } from "../../../types/model";
+
+const PostList = ({ selectedOption }: { selectedOption?: string }) => {
+
+    const { isLoading, error, data: posts } = useQuery({
+        queryKey: ['posts', selectedOption || POST_OPTION.recent],
+        queryFn: async () => axios.get(`/data/posts/${selectedOption || POST_OPTION.recent}Post.json`).then((res) => {
+            return res.data;
+        }),
+        staleTime: 5 * 60 * 1000,
+    });
+
+    if (isLoading) return <div>로딩중입니다</div>;
+
+    if (error) return <div>데이터를 받아오지 못했습니다</div>;
+
     return (
-        <div>
-            {mode === POST_MODE.recent ? "최신순" : "인기순"}
+        <div className="w-full p-6 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4" >
+            {
+                posts.map((post: Post) => {
+                    return <PostCard key={post.id} post={post} />
+                })
+
+            }
         </div>
     );
 }
