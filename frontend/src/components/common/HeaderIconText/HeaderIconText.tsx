a4a688b47a144b9155ac6ec2601d5425b33daaab
suhwa <tnghk9611@naver.com>
2024-01-02 18:02:02 +0900

    refactor: HeaderIconText 컴포넌트의 onClick props optional 하게 변경
    이슈 번호: #18

--- frontend/src/components/common/HeaderIconText/HeaderIconText.tsx
+++ frontend/src/components/common/HeaderIconText/HeaderIconText.tsx
@@ -1,4 +1,4 @@
-const HeaderIconText = ({ icon, alt, text, onClick }: { icon: string, alt: string, text: string, onClick: React.MouseEventHandler<HTMLButtonElement> }) => (
+const HeaderIconText = ({ icon, alt, text, onClick }: { icon: string, alt: string, text: string, onClick?: React.MouseEventHandler<HTMLButtonElement> }) => (
     <button className="flex justify-center items-center mr-4 cursor-pointer" type="button" onClick={onClick}>
         <img className="w-6" src={icon} alt={alt} />
         <span className="text-gray-600 font-mediun text-xs pl-1">{text}</span>
