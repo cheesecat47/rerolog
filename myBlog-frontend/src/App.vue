64ea45d9ba1d0803f262e53a078c2a6b2264dee3
cheesecat47 <cheesecat47@gmail.com>
2024-01-06 13:41:54 +0900

    Fix: getUserInfo 호출 결과를 보여주도록 Navbar 등 수정
    이슈 번호: #39

--- myBlog-frontend/src/App.vue
+++ myBlog-frontend/src/App.vue
@@ -2,6 +2,13 @@
 import { RouterView } from 'vue-router';
 import NavbarTop from './components/NavbarTop.vue';
 import TheFooter from './components/TheFooter.vue';
+import { useUserStore } from './stores/userStore.js';
+
+const userStore = useUserStore();
+
+// 블로그 방문 시 주인 유저 정보 조회
+const { getBloggerInfo } = userStore;
+getBloggerInfo('cheesecat47');
 </script>
 
 <template>
