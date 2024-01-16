fbe9199dbf4156c23c2eb6f425fbd480b76299e1
suhwa <tnghk9611@naver.com>
2024-01-16 23:25:51 +0900

    feat: 블로그 정보 api 구현
    이슈 번호: #31

---
+++ frontend/src/hooks/useBlog.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { defaultInstance } from 'apis/defaultInstance';
+import { QUERY_KEY } from 'constants/queryKeys';
+import { getBlogInfoReqest, getBlogInfoResponse } from 'interfaces/api/blog';
+
+export const useBlog = () => {
+    const staleTime = Number(process.env.REACT_APP_STALE_TIME);
+
+    const getBlogInfo = ({ userId }: getBlogInfoReqest) => {
+        return useQuery({
+            queryKey: [QUERY_KEY.BLOG, userId],
+            queryFn: async () => {
+                const response: getBlogInfoResponse = await defaultInstance
+                    .get(`/blog/${userId}`)
+                    .then((res) => res.data);
+                return response;
+            },
+            staleTime,
+        });
+    };
+
+    return { getBlogInfo };
+};
