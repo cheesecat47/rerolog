bab741ea78a04e263006e45c0894e3939b903a1f
suhwa <tnghk9611@naver.com>
2024-01-16 23:26:30 +0900

    feat: 카테고리 정보를 받는 api 구현
    이슈 번호: #깃허브이슈번호

---
+++ frontend/src/hooks/useCategory.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { defaultInstance } from 'apis/defaultInstance';
+import { QUERY_KEY } from 'constants/queryKeys';
+import {
+    getCategoryRequest,
+    getCategoryResponse,
+} from 'interfaces/api/category';
+
+export const useCategory = () => {
+    const staleTime = Number(process.env.REACT_APP_STALE_TIME);
+
+    const getCategoryList = ({ userId }: getCategoryRequest) => {
+        return useQuery({
+            queryKey: [QUERY_KEY.CATEGORY, userId],
+            queryFn: async () => {
+                const response: getCategoryResponse = await defaultInstance
+                    .get(`/blog/${userId}/category`)
+                    .then((res) => res.data);
+                return response.data;
+            },
+            staleTime,
+        });
+    };
+
+    return { getCategoryList };
+};
