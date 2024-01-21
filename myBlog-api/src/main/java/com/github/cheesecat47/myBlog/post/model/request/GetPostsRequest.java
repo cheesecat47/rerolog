03115a533a7e1504d3820c1ed757d5fcbf727e9a
cheesecat47 <cheesecat47@gmail.com>
2024-01-21 18:56:07 +0900

    Fix: getPosts 파라미터를 DTO로 변경
    이슈 번호: #86
    
    - getPosts에 쿼리 파라미터가 많았는데, 일일이 선언해서 코드가 길어지고 보기 힘들었음.
    - DTO로 바꾸고 `@ParameterObject` 어노테이션을 사용해 Swagger에서 잘 보이도록 변경

--- myBlog-api/src/main/java/com/github/cheesecat47/myBlog/post/model/request/GetPostsRequest.java
+++ myBlog-api/src/main/java/com/github/cheesecat47/myBlog/post/model/request/GetPostsRequest.java
@@ -11,12 +11,12 @@ public class GetPostsRequest {
     @Schema(description = "게시판 아이디. 특정 게시판에 속한 글만 필터링 할 때 사용. 없으면 전체 글 목록.")
     int categoryId;
 
-    @Schema(description = "latest(최신순), oldest(오래된순), popular(인기순) 중 택 1. 기본값은 latest")
-    String order;
+    @Schema(description = "`latest(최신순)`, `oldest(오래된순)`, `popular(인기순)` 중 택 1. 기본 값은 `latest`", defaultValue = "latest")
+    String order = "latest";
 
-    @Schema(description = "정렬된 결과 중 offset부터 반환. 기본값 0")
-    int offset;
+    @Schema(description = "정렬된 결과 중 offset부터 반환. 기본값 0", defaultValue = "0")
+    int offset = 0;
 
-    @Schema(description = "offset부터 limit개 조회. 기본값 10")
-    int limit;
+    @Schema(description = "offset부터 limit개 조회. 기본값 10", defaultValue = "10")
+    int limit = 10;
 }
