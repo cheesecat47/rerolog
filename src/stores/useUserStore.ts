d43b636fbc0356003a72b1027f04ce4dd25eade2
suhwa <tnghk9611@naver.com>
2024-01-31 01:59:53 +0900

    feat: user store에 accessToken, refreshToken 추가
    이슈 번호: #52

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -1,27 +1,50 @@
-import { getIsLogin, getUserId } from 'utils/localStorage';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface UserState {
     isLogin: boolean;
     userId: string;
-    loginUser: ({ userId }: { userId: string }) => void;
+    accessToken: string;
+    refreshToken: string;
+    loginUser: ({
+        userId,
+        accessToken,
+        refreshToken,
+    }: {
+        userId: string;
+        accessToken: string;
+        refreshToken: string;
+    }) => void;
     logoutUser: () => void;
 }
 
-const useUserStore = create<UserState>((set) => ({
-    isLogin: getIsLogin(),
-    userId: getUserId(),
-    loginUser: ({ userId }) => {
-        set({
-            userId,
-            isLogin: true,
-        });
-    },
-    logoutUser: () =>
-        set({
-            userId: '',
+const useUserStore = create(
+    persist<UserState>(
+        (set) => ({
             isLogin: false,
+            userId: '',
+            accessToken: '',
+            refreshToken: '',
+            loginUser: ({ userId, accessToken, refreshToken }) =>
+                set({
+                    userId,
+                    accessToken,
+                    refreshToken,
+                    isLogin: true,
+                }),
+
+            logoutUser: () =>
+                set({
+                    userId: '',
+                    accessToken: '',
+                    refreshToken: '',
+                    isLogin: false,
+                }),
         }),
-}));
+        {
+            name: 'user-store',
+        }
+    )
+);
 
 export default useUserStore;
