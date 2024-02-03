c1fefccb24ba8d00769206702cc44ddf6296b323
cheesecat47 <cheesecat47@gmail.com>
2024-02-03 13:48:12 +0900

    Add be project

--- api/myBlog-frontend/src/api/blog.js
+++
@@ -1,7 +0,0 @@
-import { localAxios } from '../util/http-commons';
-
-const local = localAxios();
-
-export const getCategories = async (id, success, fail) => {
-  local.get(`/blog/${id}/category`).then(success).catch(fail);
-};
